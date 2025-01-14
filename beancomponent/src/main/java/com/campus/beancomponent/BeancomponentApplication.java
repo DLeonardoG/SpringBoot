2d53dcdd281ea1c4fdc69ef74dbb5003ec169ed5
DLeonardoG <darkleonardog@gmail.com>
2025-01-14 11:45:03 -0500

    build: :tada: started the project with BeanComponent
    started with the first SpringBoot topic
    
    By @DLeonardoG

---
+++ beancomponent/src/main/java/com/campus/beancomponent/BeancomponentApplication.java
@@ -0,0 +1,13 @@
+package com.campus.beancomponent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeancomponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeancomponentApplication.class, args);
+	}
+
+}
---
+++ beancomponent/src/test/java/com/campus/beancomponent/BeancomponentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.beancomponent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeancomponentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
